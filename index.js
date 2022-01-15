37fb6a20dfd37cbbbb06d98716f7aa668c9ff0ea
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-15 01:55:43 +0000

    Add a function that recursively sums whole numbers in a given range

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  // Sum all the values from fromN up to toN 3-7
+  if (fromN === toN) {
+    return toN;
+  }
+  return fromN + sum(fromN + 1, toN)
 }
 
 module.exports = sum;
